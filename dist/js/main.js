5584dde4b82f495cd6f85f6aecd13fae19cb74e9
Tomasz G <tomasz.galina@gmail.com>
2017-05-31 00:53:40 +0200

    upload complete website

---
+++ dist/js/main.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+	pageAnimations.checkScroll();
+	$(window).scroll(function(){
+		pageAnimations.checkScroll();
+	});
+	$(window).resize(function(){
+		pageAnimations.checkWidth();
+	});
+});
+
+const pageAnimations = {
+	wScroll: $(window).scrollTop(),
+	wWidth: $(window).width(),
+
+	checkScroll: function(){
+		this.wScroll = $(window).scrollTop();
+		if (this.wScroll > 110){
+			this.navbarAlternate();
+		} else if (this.wWidth > 1000) {
+			this.navbarDefault();
+		}
+	},
+	checkWidth: function(){
+		this.wWidth = $(window).width();
+		if (this.wWidth < 1000){
+			this.navbarAlternate();
+		} else if (this.wScroll < 110) {
+			this.navbarDefault();
+		}
+	},
+	navbarDefault: function(){
+		$(".navbar").css({
+			"background-color" : "transparent"
+		});
+		$(".navbar-brand").css({
+			"font-size" : "1.2rem"
+		});
+		$(".navbar a").css({
+			"color" : "white"
+		});	
+	},
+	navbarAlternate: function(){
+		$(".navbar").css({
+			"background-color" : "#fff"
+		});
+		$(".navbar-brand").css({
+			"font-size" : "1rem"
+		});		
+		$(".navbar a").css({
+			"color" : "#F05F40"
+		});
+	}
+}
